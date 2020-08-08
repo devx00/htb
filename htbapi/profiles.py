89906763e433ccf3b7ef40d786665563f7c95718
Zach Hanson <zachhanson94@gmail.com>
2020-08-08 01:52:24 -0700

    Added docstring attributes to HTBProfile, HTBTeam, HTBUser

--- htbapi/profiles.py
+++ htbapi/profiles.py
@@ -10,7 +10,40 @@ from .models import HTBObject
 import json
 
 class HTBProfile(HTBObject):
-    """A HTB user's profile."""
+    """A HTB user's profile.
+    
+    Attributes:
+        id (int): The users ID.
+        name (str): The user's username.
+        timezone (str): The user's timezone.
+        isVip (bool): Whether the user is a VIP member or not.
+        avatar (str): The path to the user's avatar image.
+        points (int): The number of points the user has.
+        system_owns (int): The number of system owns the user has.
+        user_owns (int): The number of user owns the user has.
+        system_bloods (int): The number of system bloods the user has.
+        user_bloods (int): The number of user bloods the user has.
+        respects (int): The number of respects a user has.
+        country_name (str): The country the user is from. Full name.
+        country_code (str): The country the user is from. Abbreviation.
+        team (HTBTeam): The user's team.
+        university_name (str): The user's University.
+        description (str): The user's profile description.
+        github (str): A link to the user's github profile.
+        linkedin (str): A link to the user's linkedin profile.
+        twitter (str): A link to the user's twitter profile.
+        website (str): A link to the user's website.
+        isRespected (bool): Whether the authed user respects this user.
+        isFollowed (bool): Whether the authed user follows this user.
+        rank (str): The user's rank.
+        rank_id (int): The rank ID of the user's rank.
+        current_rank_progress (float): The percent progress to the next rank.
+        next_rank (str): The next rank the user can level up to.
+        next_rank_points (float): The total percent progress to the next rank.
+        rank_ownership (str): The user's total completion percentage.
+        rank_requirement (int): The percentage needed for the current rank.
+        ranking (int): The user's global Hall of Fame ranking.
+    """
 
     objectendpoint = "/user/profile/basic/"
     objectkey = "profile"
--- htbapi/user.py
+++ htbapi/user.py
@@ -12,6 +12,18 @@ class HTBUser(HTBObject):
     
     Since this class is for accessing the current user's account it is meant to
     be a singleton class and not manually instantiated. 
+
+    Attributes:
+        id (int): The user's ID.
+        name (str): The user's username.
+        email (str): The user's email.
+        timezone (str): The user's timezone.
+        isVip (bool): Whether the user is a VIP member or not.
+        canAccessVIP (bool): Whether the user has access to the VIP servers.
+        isServerVIP (bool): Whether the user's current server is a VIP server.
+        server_id (int): The ID of the user's current VPN server.
+        avatar (str): The path to the user's avatar image.
+        profile (HTBProfile): The user's profile.
     """
 
     objectendpoint = "/user/info"
